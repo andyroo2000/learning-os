03f381520d55b89f5263af2441de38676edcbfa4
Andrew Landry <andrewlandry@gmail.com>
2026-05-27 08:40:58 -0400

    Add create card API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\Flashcards\StoreCardController;
 use App\Http\Controllers\Api\Flashcards\StoreDeckController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/cards', StoreCardController::class);
 Route::post('/decks', StoreDeckController::class);

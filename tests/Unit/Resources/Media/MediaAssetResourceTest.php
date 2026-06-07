fd678eb0b92c4ac82bc67f741095b654225ee3fd
Andrew Landry <andrewlandry@gmail.com>
2026-06-07 08:07:37 -0400

    Remove cross-domain source kind test imports

--- tests/Unit/Resources/Media/MediaAssetResourceTest.php
+++ tests/Unit/Resources/Media/MediaAssetResourceTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit\Resources\Media;
 
 use App\Domain\Media\Models\MediaAsset;
-use App\Domain\Study\Models\StudyImportJob;
 use App\Http\Resources\Media\MediaAssetResource;
 use Tests\TestCase;
 
@@ -15,7 +14,7 @@ class MediaAssetResourceTest extends TestCase
         $mediaAsset->setRawAttributes([
             'id' => '01jzk7k5g9e1k8z6w3b4n9y2pa',
             'import_job_id' => '01k1j8n4st9y2aqj9b43r1dz0e',
-            'source_kind' => StudyImportJob::SOURCE_TYPE_ANKI_COLPKG,
+            'source_kind' => 'anki_import',
             'source_media_ref' => '0',
             'source_filename' => 'word.mp3',
             'disk' => 'media',
@@ -29,7 +28,7 @@ class MediaAssetResourceTest extends TestCase
         $resource = MediaAssetResource::make($mediaAsset)->resolve();
 
         $this->assertSame('01k1j8n4st9y2aqj9b43r1dz0e', $resource['import_job_id']);
-        $this->assertSame(StudyImportJob::SOURCE_TYPE_ANKI_COLPKG, $resource['source_kind']);
+        $this->assertSame('anki_import', $resource['source_kind']);
         $this->assertSame('0', $resource['source_media_ref']);
         $this->assertSame('word.mp3', $resource['source_filename']);
         $this->assertArrayNotHasKey('disk', $resource);

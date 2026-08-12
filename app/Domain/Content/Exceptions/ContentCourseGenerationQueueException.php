9780cd65b1bd9cfbfb7cf9863aef35c906b93217
Andrew Landry <andrewlandry@gmail.com>
2026-08-12 14:01:01 -0400

    Preserve admin course queue compatibility

--- app/Domain/Content/Exceptions/ContentCourseGenerationQueueException.php
+++ app/Domain/Content/Exceptions/ContentCourseGenerationQueueException.php
@@ -9,7 +9,7 @@ final class ContentCourseGenerationQueueException extends RuntimeException
 {
     public function __construct(
         string $message,
-        public readonly string $clientRequestId,
+        public readonly ?string $clientRequestId = null,
         ?Throwable $previous = null,
     ) {
         parent::__construct($message, previous: $previous);

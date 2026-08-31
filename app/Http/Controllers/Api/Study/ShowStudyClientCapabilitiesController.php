8bbddeb9d27f9f0904b6ace7133e3b87a0dd7ba0
Andrew Landry <andrewlandry@gmail.com>
2026-08-31 13:04:06 -0400

    Define the capabilities response resource

--- app/Http/Controllers/Api/Study/ShowStudyClientCapabilitiesController.php
+++ app/Http/Controllers/Api/Study/ShowStudyClientCapabilitiesController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api\Study;
 
 use App\Domain\Study\Support\StudyClientCapabilities;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Study\StudyClientCapabilitiesResource;
 use Illuminate\Http\JsonResponse;
 
 final class ShowStudyClientCapabilitiesController extends Controller
 {
     public function __invoke(StudyClientCapabilities $capabilities): JsonResponse
     {
-        return response()->json($capabilities->toArray());
+        return StudyClientCapabilitiesResource::make($capabilities)->response();
     }
 }
